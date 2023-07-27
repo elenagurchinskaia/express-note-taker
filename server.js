06a279a62572b4def4ccf9593817f80d484e82ec
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-27 17:12:23 -0500

    added starter pseudocode

---
+++ server.js
@@ -0,0 +1,15 @@
+// import required packages
+// refer to the PORT
+
+// boiler plate info
+// set route to dp.json file
+// > GET /api/notes
+// route for index.html
+// > GET *
+// route to note.html
+// > GET/notes
+// route for post = when used wants to make a new note
+// > POST/api/notes
+// optional: route for delete > delete posted info based on ID
+// > DELETE /api/notes/:id
+// listen on port
