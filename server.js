ea24000cb9952098b5c05b6798574b5b6470112e
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-30 20:19:18 -0500

    updated server.js code

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-// import required packages
+// ================================  Import required packages  ================================== //
+const express = require("express");
+const path = require("path");
+const app = express();
+const api = require("./routes/index.js");
+const PORT = 3000;
 // refer to the PORT
 
 // boiler plate info
 // set route to dp.json file
 // > GET /api/notes
-// route for index.html
+
+// ================================  Middleware to parse incoming data  ================================== //
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+app.use(express.static("public"));
+
+// ================================  Global array to store notes  ================================== //
+
+let notes = [];
+
+// ================================  Routes  ================================== //
+// route for index.html (Landing Page)
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 // > GET *
 // route to note.html
 // > GET/notes
@@ -13,3 +33,16 @@
 // optional: route for delete > delete posted info based on ID
 // > DELETE /api/notes/:id
 // listen on port
+
+// server-side
+// > const from activities
+
+// ================================  Import public folder, html routes  ================================== //
+
+// look at 22 solved
+
+// ================================  Main server listen  ================================== //
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
