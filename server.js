adf4510318f9f57fc03ca0a86936f66c6cf4c35f
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-30 22:01:43 -0500

    added GET routes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // ================================  Import required packages  ================================== //
+// require
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
-const app = express();
+
 const api = require("./routes/index.js");
+// instance of express
+const app = express();
 const PORT = 3000;
 // refer to the PORT
 
@@ -13,21 +17,31 @@ const PORT = 3000;
 // ================================  Middleware to parse incoming data  ================================== //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
 app.use(express.static("public"));
+app.use("/api", api);
 
 // ================================  Global array to store notes  ================================== //
 
 let notes = [];
 
-// ================================  Routes  ================================== //
+// ================================  File path for the db.json file  ================================== //
+
+const dbFilePath = path.join(__dirname, "db", "db.json");
+
+// ================================  GET Routes  ================================== //
+
 // route for index.html (Landing Page)
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
 // > GET *
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
+});
 // route to note.html
 // > GET/notes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/assets/notes.html"));
+});
+// route for saved notes as json
+
 // route for post = when used wants to make a new note
 // > POST/api/notes
 // optional: route for delete > delete posted info based on ID
@@ -43,6 +57,6 @@ app.get("/", (req, res) => {
 
 // ================================  Main server listen  ================================== //
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
